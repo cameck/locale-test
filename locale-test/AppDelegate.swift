399a94cbe014d419b558c93959473f081f53cf37
Cameron Eckelberry <ceckelberry@malwarebytes.com>
2021-10-25 13:27:38 -0400

    Initial Commit

---
+++ locale-test/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  locale-test
+//
+//  Created by Cameron Eckelberry on 10/25/21.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Override point for customization after application launch.
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
